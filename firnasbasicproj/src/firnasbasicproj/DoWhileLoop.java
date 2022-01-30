c65aab7adb6548dc9d038f2f7e2971e2f571ce86
PC <PC@PC-PC>
2022-01-30 21:12:42 +0530

    doubt to DoWhileLoop

---
+++ firnasbasicproj/src/firnasbasicproj/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package firnasbasicproj;
+
+public class DoWhileLoop {
+	int x = 1;//doubt
+	do {
+		System.out.println("Value of x :" + x);
+		x++;
+		System.out.println("\n");
+	}while (x < 11);
+	}
+
+}
