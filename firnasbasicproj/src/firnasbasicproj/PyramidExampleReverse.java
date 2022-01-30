6a644bce7ac6a5c8040fb8dd72facd30938add80
PC <PC@PC-PC>
2022-01-30 20:39:25 +0530

    doubt to PyramidExampleReverse

---
+++ firnasbasicproj/src/firnasbasicproj/PyramidExampleReverse.java
@@ -0,0 +1,16 @@
+package firnasbasicproj;
+
+public class PyramidExampleReverse {
+	public static void main(String[] args) {
+		int term=6;
+		for(int i=1;i<=terml;i++)
+		{
+					for(int j=term;j>=i;j--)
+					{
+						System.out.println("* ");					
+					}
+					System.out.println();//new line
+		}
+	}
+
+}
