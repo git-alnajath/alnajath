08360af06b3b6902d862c0e37f9d883783df96fa
PC <PC@PC-PC>
2022-01-31 13:38:18 +0530

    CheckPrime Learned

---
+++ firnasbasicproj/src/firnasbasicproj/CheckPrime.java
@@ -0,0 +1,29 @@
+package firnasbasicproj;
+import java.util.Scanner;
+public class CheckPrime {
+	public static void main(String[] args) {
+		int temp;
+		boolean isPrime=true;
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter any number :");
+		//Captured the input in an integer
+		int num=scan.nextInt();
+			scan.close();
+		for(int i=2;i<=num/2;i++)
+		{
+				temp=num%i;
+			if(temp==0)
+			{
+				isPrime=false;
+				break;
+			}
+		}
+		//if isPrime is true the number is prime else not
+		if(isPrime)
+			System.out.println(num +" is a prime Number");
+		else
+			System.out.println(num +" is not a prime Number ");
+	}
+
+}
+
